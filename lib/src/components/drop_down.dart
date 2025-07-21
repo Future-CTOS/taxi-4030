b1bc83fcad5b8f6bc6fa193b180606ef8b813cca
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-21 18:09:02 +0330

    refactor: change some necessary fields of dropdown

--- lib/src/components/drop_down.dart
+++ lib/src/components/drop_down.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:taxi_4030/src/infrastructures/theme/spacing.dart';
+import 'package:taxi_4030/src/infrastructures/utils/spacing.dart';
 
 typedef ItemAsString<T> = String Function(T item);
 typedef OnChanged<T> = void Function(T?)?;
@@ -7,7 +7,7 @@ typedef OnChanged<T> = void Function(T?)?;
 class CustomDropDown<T> extends StatefulWidget {
   const CustomDropDown({
     super.key,
-    required this.title,
+    this.title,
     required this.items,
     required this.getTitle,
     required this.value,
@@ -15,7 +15,7 @@ class CustomDropDown<T> extends StatefulWidget {
     this.onClear,
   });
 
-  final String title;
+  final String? title;
   final List<T> items;
   final ItemAsString<T> getTitle;
   final OnChanged<T>? onSelectItem;
@@ -29,81 +29,80 @@ class CustomDropDown<T> extends StatefulWidget {
 class _CustomDropDownState<T> extends State<CustomDropDown<T>> {
   @override
   Widget build(BuildContext context) => Padding(
-        padding: AppSpacing.mediumPadding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize:  AppSpacing.largeSpace,
-              ),
+    padding: AppSpacing.mediumPadding,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.title != null) ...[
+          Text(
+            widget.title!,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: AppSpacing.largeSpace,
             ),
-            AppSpacing.mediumVerticalSpacer,
-            SizedBox(
-              width: double.infinity,
-              height: 50,
-              child: _decoration(),
-            ),
-          ],
-        ),
-      );
+          ),
+          AppSpacing.mediumVerticalSpacer,
+        ],
+        SizedBox(width: double.infinity, height: 44, child: _decoration()),
+      ],
+    ),
+  );
 
   Widget _decoration() => DecoratedBox(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 0.5,
-            strokeAlign: BorderSide.strokeAlignOutside,
-            color: Colors.white,
-          ),
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(AppSpacing.mediumSpace),
-          ),
-        ),
-        child: _dropdown(),
-      );
+    decoration: BoxDecoration(
+      border: Border.all(
+        width: 1,
+        strokeAlign: BorderSide.strokeAlignOutside,
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+      borderRadius: const BorderRadius.all(
+        Radius.circular(AppSpacing.mediumSpace),
+      ),
+    ),
+    child: _dropdown(),
+  );
 
   Widget _dropdown() => DropdownButtonHideUnderline(
-        child: DropdownButton<T>(
-          iconEnabledColor: Colors.white,
-          icon: widget.value != null
-              ? InkWell(
-                  onTap: widget.onClear,
-                  child: Icon(
-                    Icons.clear,
-                    size: AppSpacing.xLargeSpace,
-                    color: Theme.of(context).colorScheme.primary,
+    child: DropdownButton<T>(
+      iconEnabledColor: Colors.white,
+      icon: widget.value != null
+          ? InkWell(
+              onTap: widget.onClear,
+              child: Icon(
+                Icons.clear,
+                size: AppSpacing.xLargeSpace,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            )
+          : Icon(
+              Icons.keyboard_arrow_down,
+              size: AppSpacing.xLargeSpace,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+      menuMaxHeight: 200,
+      dropdownColor: Theme.of(context).colorScheme.secondary,
+      value: widget.value,
+      onChanged: (T? value) {
+        if (widget.onSelectItem != null) {
+          widget.onSelectItem!(value);
+        }
+      },
+      items: <T>[...widget.items]
+          .map<DropdownMenuItem<T>>(
+            (T value) => DropdownMenuItem<T>(
+              value: value,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.getTitle(value),
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
                   ),
-                )
-              : Icon(
-                  Icons.keyboard_arrow_down,
-                  size: AppSpacing.xLargeSpace,
-                  color: Theme.of(context).colorScheme.primary,
                 ),
-          menuMaxHeight: 200,
-          dropdownColor: Theme.of(context).colorScheme.primary,
-          value: widget.value,
-          onChanged: (T? value) {
-            if (widget.onSelectItem != null) {
-              widget.onSelectItem!(value);
-            }
-          },
-          items: <T>[...widget.items]
-              .map<DropdownMenuItem<T>>(
-                (T value) => DropdownMenuItem<T>(
-                  value: value,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      widget.getTitle(value),
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              )
-              .toList(),
-        ),
-      );
+              ),
+            ),
+          )
+          .toList(),
+    ),
+  );
 }
