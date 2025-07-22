a84e9bb56d6f74651c5df6cef175f2693557585d
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-22 15:43:36 +0330

    feat: Add onChange method on textFiled

--- lib/src/components/text_field.dart
+++ lib/src/components/text_field.dart
@@ -7,6 +7,7 @@ class CustomTextField extends StatefulWidget {
     super.key,
     required this.textController,
     this.title,
+    this.onChange,
     this.isRequired = false,
     this.isPassword = false,
     this.isNumber = false,
@@ -23,6 +24,7 @@ class CustomTextField extends StatefulWidget {
   final String? title;
   final bool? isPassword;
   final TextEditingController textController;
+  final Function(String)? onChange;
   final bool? isRequired;
   final GlobalKey<FormState>? formKey;
   final String? hint;
@@ -40,69 +42,73 @@ class _CustomTextFieldState extends State<CustomTextField> {
   bool _obscureText = false;
 
   @override
-  Widget build(BuildContext context) => Padding(
-    padding: AppSpacing.mediumPadding,
-    child: Form(
-      key: widget.formKey,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 1,
-            strokeAlign: BorderSide.strokeAlignOutside,
-            color: Theme.of(context).colorScheme.secondary,
+  Widget build(BuildContext context) =>
+      Form(
+        key: widget.formKey,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              strokeAlign: BorderSide.strokeAlignOutside,
+              color: Theme
+                  .of(context)
+                  .colorScheme
+                  .secondary,
+            ),
+            color: Theme
+                .of(context)
+                .colorScheme
+                .surface,
+            borderRadius: const BorderRadius.all(
+              Radius.circular(AppSpacing.mediumSpace),
+            ),
           ),
-          color: Theme.of(context).colorScheme.surface,
-          borderRadius: const BorderRadius.all(
-            Radius.circular(AppSpacing.mediumSpace),
-          ),
-        ),
-        child: TextFormField(
-          readOnly: widget.readOnly!,
-          validator: widget.isRequired!
-              ? (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'required';
-                  }
-                  return null;
-                }
-              : (value) => null,
-          maxLines: widget.maxLines ?? 1,
-          controller: widget.textController,
-          obscureText: _getVisibility(_obscureText, widget.isPassword ?? false),
-          showCursor: true,
-
-          decoration: InputDecoration(
-            suffixIcon: widget.suffixIcon != null
-                ? Icon(widget.suffixIcon!, color: Colors.white70)
-                : widget.isPassword!
-                ? IconButton(
-                    onPressed: () =>
-                        setState(() => _obscureText = !_obscureText),
-                    icon: _obscureText
-                        ? const Icon(Icons.visibility_off, color: Colors.white)
-                        : const Icon(Icons.visibility, color: Colors.white),
-                  )
+          child: TextFormField(
+            readOnly: widget.readOnly!,
+            validator: widget.isRequired!
+                ? (value) {
+              if (value == null || value.isEmpty) {
+                return 'required';
+              }
+              return null;
+            }
+                : (value) => null,
+            maxLines: widget.maxLines ?? 1,
+            controller: widget.textController,
+            obscureText: _getVisibility(
+                _obscureText, widget.isPassword ?? false),
+            showCursor: true,
+            onChanged: widget.onChange,
+            decoration: InputDecoration(
+              suffixIcon: widget.suffixIcon != null
+                  ? Icon(widget.suffixIcon!, color: Colors.white70)
+                  : widget.isPassword!
+                  ? IconButton(
+                onPressed: () =>
+                    setState(() => _obscureText = !_obscureText),
+                icon: _obscureText
+                    ? const Icon(Icons.visibility_off, color: Colors.white)
+                    : const Icon(Icons.visibility, color: Colors.white),
+              )
+                  : null,
+              border: const OutlineInputBorder(borderSide: BorderSide.none),
+              hintText: widget.hint,
+              hintStyle: TextStyle(fontSize: 20),
+            ),
+            inputFormatters: widget.isNumber!
+                ? <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly]
                 : null,
-            border: const OutlineInputBorder(borderSide: BorderSide.none),
-            hintText: widget.hint,
-            hintStyle: TextStyle(fontSize: 20),
+            keyboardType: widget.isNumber! ? TextInputType.number : null,
+            maxLength: widget.maxLength,
+            buildCounter:
+                (context, {
+              required currentLength,
+              required isFocused,
+              required maxLength,
+            }) => null,
           ),
-          inputFormatters: widget.isNumber!
-              ? <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly]
-              : null,
-          keyboardType: widget.isNumber! ? TextInputType.number : null,
-          maxLength: widget.maxLength,
-          buildCounter:
-              (
-                context, {
-                required currentLength,
-                required isFocused,
-                required maxLength,
-              }) => null,
         ),
-      ),
-    ),
-  );
+      );
 
   bool _getVisibility(bool isHidden, bool isPassword) {
     if (!isPassword) {
