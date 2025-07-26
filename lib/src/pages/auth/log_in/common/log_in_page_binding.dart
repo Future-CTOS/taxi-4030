193b07bb8afbee0c4275b67feaf4b6ba2636e360
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-26 18:38:31 +0330

    redesign: delete login & signup contents

--- lib/src/pages/auth/log_in/common/log_in_page_binding.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-
-import '../controller/log_in_page_controller.dart';
-
-class LogInPageBinding extends Bindings {
-  @override
-  void dependencies() => Get.lazyPut(LogInPageController.new);
-}
