edc4a57b6a8fa322868354e80313d2f8b2d1d9da
shayanzare-dev <shayanzare2222@gmail.com>
2025-07-26 18:38:00 +0330

    redesign: edit custom loading

--- lib/src/components/loading.dart
+++ lib/src/components/loading.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomLoading extends StatelessWidget {
-  const CustomLoading({
-    this.value,
-    super.key,
-  });
+  const CustomLoading({this.value, super.key});
 
   final double? value;
 
   @override
   Widget build(BuildContext context) => Center(
-        child: CircularProgressIndicator(
-          value: value,
-          valueColor: const AlwaysStoppedAnimation<Color>(Colors.white70),
-        ),
-      );
+    child: CircularProgressIndicator(
+      value: value,
+      valueColor: AlwaysStoppedAnimation<Color>(
+        Theme.of(context).colorScheme.onPrimary,
+      ),
+    ),
+  );
 }
