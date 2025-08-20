10e52b639778748546731999dd00249941a1d700
shayanzare-dev <shayanzare2222@gmail.com>
2025-08-21 00:39:54 +0330

    chore: Remove Deprecated imports and ways of using packages

--- lib/src/pages/driver_auth/video_auth/controller/video_auth_controller.dart
+++ lib/src/pages/driver_auth/video_auth/controller/video_auth_controller.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
@@ -33,25 +31,25 @@ class VideoAuthController extends GetxController {
       return;
     }
 
-    final String? compressedPath = await _compressVideo(
-      inputPath: pickedFile.path,
-      // outputFileName: "auth_video.mp4",
-      crf: 28,
-      maxWidth: 720,
-    );
+    // final String? compressedPath = await _compressVideo(
+    //   inputPath: pickedFile.path,
+    //   // outputFileName: "auth_video.mp4",
+    //   crf: 28,
+    //   maxWidth: 720,
+    // );
+    //
+    // if (compressedPath == null) {
+    //   isLoading.value = false;
+    //   Utils.showSnackBar(
+    //     context,
+    //     text: "خطا در فشرده‌سازی ویدیو",
+    //     status: StatusEnum.danger,
+    //   );
+    //   return;
+    // }
 
-    if (compressedPath == null) {
-      isLoading.value = false;
-      Utils.showSnackBar(
-        context,
-        text: "خطا در فشرده‌سازی ویدیو",
-        status: StatusEnum.danger,
-      );
-      return;
-    }
-
-    final File compressedFile = File(compressedPath);
-    final Uint8List bytes = await compressedFile.readAsBytes();
+    // final File compressedFile = File(compressedPath);
+    final Uint8List bytes = await pickedFile.readAsBytes();
 
     final resultOrException = await _repository.uploadAuthVideo(
       bytes: bytes,
@@ -73,47 +71,6 @@ class VideoAuthController extends GetxController {
     );
   }
 
-  Future<String?> _compressVideo({
-    required String inputPath,
-    String? outputFileName,
-    int crf = 28,
-    int maxWidth = 720,
-  }) async {
-    try {
-      String outputPath;
-      if (kIsWeb) {
-        outputPath = '/tmp/${outputFileName ?? "compressed.mp4"}';
-      } else {
-        final Directory tempDir = await getTemporaryDirectory();
-        outputPath = path.join(
-          tempDir.path,
-          outputFileName ?? 'compressed.mp4',
-        );
-      }
-      final Directory tempDir = await getTemporaryDirectory();
-      final String fileName =
-          outputFileName ?? 'compressed_${path.basename(inputPath)}';
-      outputPath = path.join(tempDir.path, fileName);
-
-      final String command =
-          "-i '$inputPath' -vf 'scale=min($maxWidth\\,iw):-2' -c:v libx264 -crf $crf -preset fast -c:a aac -b:a 128k '$outputPath'";
-
-      final session = await FFmpegKit.execute(command);
-
-      final returnCode = await session.getReturnCode();
-      if (ReturnCode.isSuccess(returnCode)) {
-        return outputPath;
-      } else {
-        final logs = await session.getAllLogs();
-        print("FFmpeg error logs:");
-        logs.forEach((log) => print(log.getMessage()));
-        return null;
-      }
-    } catch (e) {
-      print("Error compressing video: $e");
-      return null;
-    }
-  }
 
   void onContinueTap() {
     if (!isActiveContinue.value) return;
